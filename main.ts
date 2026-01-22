15b8d3d7a8e1978d40748d471a2bda26a5a85e33
Gerard Fornés <gerard.fornes@gracia.lasalle.cat>
2026-01-22 18:09:29 +0100

    tanque

--- main.ts
+++ main.ts
@@ -1,31 +1,48 @@
-controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-right
-            `, 500, false)
+/**
+ * 1. PRIMER: Creem les variables i els personatges
+ */
+// Animació Dreta
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-right`,
+    500,
+    false
+    )
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
-    animation.runImageAnimation(nena, assets.animation`
-            nena-animation-left
-            `, 500, false)
+// Animació Esquerra
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    nena,
+    assets.animation`nena-animation-left`,
+    500,
+    false
+    )
 })
-//  Boton saltar + gravetat
-function on_a_pressed() {
+// 5. ARA SÍ: Els controls i animacions (un cop la nena ja existeix)
+// Funció de saltar
+function on_a_pressed () {
+    // Només salta si toca el terra
     if (nena.isHittingTile(CollisionDirection.Bottom)) {
         nena.vy = -150
     }
-    
 }
-
-let nena : Sprite = null
-tiles.setCurrentTilemap(tilemap`
-    prova
-    `)
-nena = sprites.create(assets.image`
-    nena-front
-    `, SpriteKind.Player)
+let nena: Sprite = null
+// Posem el mapa
+tiles.setCurrentTilemap(tilemap`prova`)
+// 2. Creem la NENA
+nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+// 2. Creem la NENA
+nena = sprites.create(assets.image`tanque`, SpriteKind.Enemy)
+// Gravetat
 nena.ay = 350
+// Només es mou esquerra/dreta amb tecles
 controller.moveSprite(nena, 100, 0)
 nena.setStayInScreen(true)
+// IMPORTANT: Això fa que la nena es pinti PER SOBRE de tot
+nena.z = 10
+// 4. Càmera
+scene.cameraFollowSprite(nena)
 controller.A.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
 controller.up.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
-scene.cameraFollowSprite(nena)
+let mySprite20260122T170716803Z = 0
