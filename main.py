3a367c44867e8733fccf2b95dc5f0179c8020901
Pol González Arribas <polgonzarri@gmail.com>
2026-01-22 08:28:57 +0100

    game over

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def on_right_pressed():
         assets.animation("""
             maduro-right0
             """),
-        500,
+        200,
         True)
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
@@ -12,7 +12,7 @@ def on_left_pressed():
         assets.animation("""
             maduro-left
             """),
-        500,
+        200,
         True)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
@@ -20,9 +20,12 @@ controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 def on_a_pressed():
     if nena.is_hitting_tile(CollisionDirection.BOTTOM):
         nena.vy = -150
+bot_mirando_derecha = False
+velocidad3 = 0
+distancia3 = 0
+nena: Sprite = None
 velocidad2 = 0
 distancia2 = 0
-nena: Sprite = None
 distancia = 0
 velocidad = 0
 tiles.set_current_tilemap(tilemap("""
@@ -34,6 +37,8 @@ bot = sprites.create(assets.image("""
 nena = sprites.create(assets.image("""
     maduro
     """), SpriteKind.player)
+tiles.place_on_tile(nena, tiles.get_tile_location(6, 14))
+tiles.place_on_tile(bot, tiles.get_tile_location(1, 10))
 nena.ay = 350
 controller.move_sprite(nena, 100, 0)
 nena.set_stay_in_screen(True)
@@ -42,36 +47,41 @@ controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 scene.camera_follow_sprite(nena)
 
-bot_mirando_derecha = False
-
-def on_update():
-    global bot_mirando_derecha
-    
-    distancia = abs(nena.x - bot.x)
-    velocidad = 0
-    if distancia > 120:
-        velocidad = 300
-    elif distancia > 60:
-        velocidad = 170
+def on_on_update():
+    global distancia3, velocidad3, bot_mirando_derecha
+    distancia3 = abs(nena.x - bot.x)
+    if distancia3 > 120:
+        velocidad3 = 300
+    elif distancia3 > 60:
+        velocidad3 = 170
     else:
-        velocidad = 95
-
+        velocidad3 = 95
     if nena.x < bot.x:
-        bot.vx = -velocidad
-        
+        bot.vx = 0 - velocidad3
         if bot_mirando_derecha == True:
-            animation.run_image_animation(bot, assets.animation("""soldado-left"""), 200, True)
+            animation.run_image_animation(bot,
+                assets.animation("""
+                    soldado-left
+                    """),
+                200,
+                True)
             bot_mirando_derecha = False
-
     else:
-        bot.vx = velocidad
-        
+        bot.vx = velocidad3
         if bot_mirando_derecha == False:
-            animation.run_image_animation(bot, assets.animation("""soldado-right"""), 200, True)
+            animation.run_image_animation(bot,
+                assets.animation("""
+                    soldado-right
+                    """),
+                200,
+                True)
             bot_mirando_derecha = True
-
     if bot.is_hitting_tile(CollisionDirection.LEFT) or bot.is_hitting_tile(CollisionDirection.RIGHT):
         if bot.is_hitting_tile(CollisionDirection.BOTTOM):
             bot.vy = -150
+game.on_update(on_on_update)
 
-game.on_update(on_update)
+def on_overlap(sprite, otherSprite):
+    game.over(False)
+
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_overlap)
--- main.ts
+++ main.ts
@@ -1,36 +1,40 @@
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`maduro-right0`,
-    200,
-    true
-    )
+controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            maduro-right0
+            `, 200, true)
 })
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    animation.runImageAnimation(
-    nena,
-    assets.animation`maduro-left`,
-    200,
-    true
-    )
+controller.left.onEvent(ControllerButtonEvent.Pressed, function on_left_pressed() {
+    animation.runImageAnimation(nena, assets.animation`
+            maduro-left
+            `, 200, true)
 })
-// Boton saltar + gravetat
-function on_a_pressed () {
+//  Boton saltar + gravetat
+function on_a_pressed() {
     if (nena.isHittingTile(CollisionDirection.Bottom)) {
         nena.vy = -150
     }
+    
 }
+
 let bot_mirando_derecha = false
 let velocidad3 = 0
 let distancia3 = 0
-let nena: Sprite = null
-let velocidad = 0
-let distancia = 0
-let distancia2 = 0
+let nena : Sprite = null
 let velocidad2 = 0
-tiles.setCurrentTilemap(tilemap`prova`)
-let bot = sprites.create(assets.image`soldado`, SpriteKind.Enemy)
-nena = sprites.create(assets.image`maduro`, SpriteKind.Player)
+let distancia2 = 0
+let distancia = 0
+let velocidad = 0
+tiles.setCurrentTilemap(tilemap`
+    prova
+    `)
+let bot = sprites.create(assets.image`
+    soldado
+    `, SpriteKind.Enemy)
+nena = sprites.create(assets.image`
+    maduro
+    `, SpriteKind.Player)
+tiles.placeOnTile(nena, tiles.getTileLocation(6, 14))
+tiles.placeOnTile(bot, tiles.getTileLocation(1, 10))
 nena.ay = 350
 controller.moveSprite(nena, 100, 0)
 nena.setStayInScreen(true)
@@ -38,7 +42,8 @@ bot.ay = 350
 controller.A.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
 controller.up.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
 scene.cameraFollowSprite(nena)
-game.onUpdate(function () {
+game.onUpdate(function on_on_update() {
+    
     distancia3 = Math.abs(nena.x - bot.x)
     if (distancia3 > 120) {
         velocidad3 = 300
@@ -47,32 +52,35 @@ game.onUpdate(function () {
     } else {
         velocidad3 = 95
     }
+    
     if (nena.x < bot.x) {
         bot.vx = 0 - velocidad3
         if (bot_mirando_derecha == true) {
-            animation.runImageAnimation(
-            bot,
-            assets.animation`soldado-left`,
-            200,
-            true
-            )
+            animation.runImageAnimation(bot, assets.animation`
+                    soldado-left
+                    `, 200, true)
             bot_mirando_derecha = false
         }
+        
     } else {
         bot.vx = velocidad3
         if (bot_mirando_derecha == false) {
-            animation.runImageAnimation(
-            bot,
-            assets.animation`soldado-right`,
-            200,
-            true
-            )
+            animation.runImageAnimation(bot, assets.animation`
+                    soldado-right
+                    `, 200, true)
             bot_mirando_derecha = true
         }
+        
     }
+    
     if (bot.isHittingTile(CollisionDirection.Left) || bot.isHittingTile(CollisionDirection.Right)) {
         if (bot.isHittingTile(CollisionDirection.Bottom)) {
             bot.vy = -150
         }
+        
     }
+    
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    game.over(false)
 })
