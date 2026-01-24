b1725cee0c90c295de133c795c02e3e1fa909cc3
Pol González Arribas <polgonzarri@gmail.com>
2026-01-25 00:26:06 +0100

    lvl 1

--- main.py
+++ main.py
@@ -5,41 +5,39 @@ class SpriteKind:
 
 tiles.set_current_tilemap(tilemap("""prova"""))
 
-# Variables Globales
 bot_mirando_derecha = False
 velocidad3 = 0
 distancia3 = 0
 nena: Sprite = None
+juego_empezado = False
 
-# Tanques
+# OBJETOS FIJOS
 tanque = sprites.create(assets.image("""tanque"""), SpriteKind.Obstacle)
 tiles.place_on_tile(tanque, tiles.get_tile_location(116, 10))
-
 tanque02 = sprites.create(assets.image("""tanque"""), SpriteKind.Obstacle)
 tiles.place_on_tile(tanque02, tiles.get_tile_location(146, 10))
 
-# Bot
+# PERSONAJES
 bot = sprites.create(assets.image("""soldado0"""), SpriteKind.enemy)
 tiles.place_on_tile(bot, tiles.get_tile_location(1, 7))
 bot.ay = 350
 
-# Maduro
 nena = sprites.create(assets.image("""maduro"""), SpriteKind.player)
 tiles.place_on_tile(nena, tiles.get_tile_location(6, 9))
 nena.ay = 350
 nena.set_stay_in_screen(True)
 scene.camera_follow_sprite(nena)
 
-
-# Minas
+# GENERADOR DE MINAS
 lista_minas = tiles.get_tiles_by_type(assets.tile("""interrogacion"""))
-for i in range(len(lista_minas)):
+i = 0
+while i < len(lista_minas):
     lugar = lista_minas[i]
     nueva_minita = sprites.create(assets.image("""minita3"""), SpriteKind.enemy)
     tiles.place_on_tile(nueva_minita, lugar)
-    tiles.set_tile_at(lugar, assets.tile("""transparency16"""))
+    i += 1
 
-#Salto Toldos
+# GENERADOR DE TOLDOS
 partes_toldo = [
     assets.tile("""toldo01"""),
     assets.tile("""toldo02"""),
@@ -47,33 +45,43 @@ partes_toldo = [
     assets.tile("""toldo04""")
 ]
 
-for t in range(len(partes_toldo)):
+t = 0
+while t < len(partes_toldo):
     tipo_actual = partes_toldo[t]
-    
     lista_lugares_toldo = tiles.get_tiles_by_type(tipo_actual)
-    
-    for k in range(len(lista_lugares_toldo)):
+    k = 0
+    while k < len(lista_lugares_toldo):
         lugar_t = lista_lugares_toldo[k]
-        
         nuevo_toldo = sprites.create(tipo_actual, SpriteKind.Trampolin)
         tiles.place_on_tile(nuevo_toldo, lugar_t)
-        
         tiles.set_tile_at(lugar_t, assets.tile("""transparency16"""))
-
+        k += 1
+    t += 1
 
 # CUENTA REGRESIVA
-for k in range(3):
+controller.move_sprite(nena, 0, 0)
+
+k = 0
+while k < 3:
     numero = 3 - k
     if nena:
-        nena.say_text("" + str(numero), 1000, True)
+        nena.say_text(str(numero), 1000, True)
     pause(1000)
+    k += 1
 
 if nena:
     nena.say_text("¡CORRE!", 500, True)
 
+juego_empezado = True
 controller.move_sprite(nena, 100, 0)
 
+if controller.right.is_pressed():
+    animation.run_image_animation(nena, assets.animation("""maduro-right0"""), 200, True)
+elif controller.left.is_pressed():
+    animation.run_image_animation(nena, assets.animation("""maduro-left"""), 200, True)
 
+
+# CONTROLES
 def on_right_pressed():
     if nena:
         animation.run_image_animation(nena, assets.animation("""maduro-right0"""), 200, True)
@@ -90,10 +98,47 @@ def on_a_pressed():
 controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
+
+# LÓGICA IA + SUELOS
+
+tiles_petroleo = [
+    assets.tile("""petroleo0"""),
+    assets.tile("""petroleo02"""),
+    assets.tile("""petroleo1""")
+]
+tile_mina = assets.tile("""interrogacion""")
+
 def on_on_update():
     global distancia3, velocidad3, bot_mirando_derecha
     if not nena or not bot: return
 
+    if juego_empezado:
+        loc_actual = nena.tilemap_location()
+        columna = loc_actual.column
+        fila_abajo = loc_actual.row + 1
+        ubicacion_suelo = tiles.get_tile_location(columna, fila_abajo)
+        
+        imagen_suelo = tiles.tile_image_at_location(ubicacion_suelo)
+        
+        # 1. MINAS
+        if imagen_suelo == tile_mina:
+            game.over(False)
+            return
+
+        # 2. PETRÓLEO
+        estoy_en_petroleo = False
+        for aceite in tiles_petroleo:
+            if imagen_suelo == aceite:
+                estoy_en_petroleo = True
+                break
+        
+        if nena.is_hitting_tile(CollisionDirection.BOTTOM):
+            if estoy_en_petroleo:
+                controller.move_sprite(nena, 20, 0)
+            else:
+                controller.move_sprite(nena, 100, 0)
+
+    # IA DEL BOT
     distancia3 = abs(nena.x - bot.x)
     if distancia3 > 90:
         velocidad3 = 320
@@ -116,19 +161,20 @@ def on_on_update():
     if bot.is_hitting_tile(CollisionDirection.LEFT) or bot.is_hitting_tile(CollisionDirection.RIGHT):
         if bot.is_hitting_tile(CollisionDirection.BOTTOM):
             bot.vy = -155
+
 game.on_update(on_on_update)
 
-def on_enemy_overlap(sprite, otherSprite):
+# COLISIONES
+
+def on_enemy_overlap(sprite2, otherSprite2):
     game.over(False)
 sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_enemy_overlap)
 
 def on_toldo_overlap(sprite, otherSprite):
     if nena:
         nena.vy = -250
-        
         if nena.vx > 0:
             nena.vx = 250
         else:
             nena.vx = -250
-            
 sprites.on_overlap(SpriteKind.player, SpriteKind.Trampolin, on_toldo_overlap)
--- main.ts
+++ main.ts
@@ -12,60 +12,75 @@ namespace SpriteKind {
 }
 
 tiles.setCurrentTilemap(tilemap`prova`)
-//  Variables Globales
 let bot_mirando_derecha = false
 let velocidad3 = 0
 let distancia3 = 0
 let nena : Sprite = null
-//  Tanques
+let juego_empezado = false
+//  OBJETOS FIJOS
 let tanque = sprites.create(assets.image`tanque`, SpriteKind.Obstacle)
 tiles.placeOnTile(tanque, tiles.getTileLocation(116, 10))
 let tanque02 = sprites.create(assets.image`tanque`, SpriteKind.Obstacle)
 tiles.placeOnTile(tanque02, tiles.getTileLocation(146, 10))
-//  Bot
+//  PERSONAJES
 let bot = sprites.create(assets.image`soldado0`, SpriteKind.Enemy)
 tiles.placeOnTile(bot, tiles.getTileLocation(1, 7))
 bot.ay = 350
-//  Maduro
 nena = sprites.create(assets.image`maduro`, SpriteKind.Player)
 tiles.placeOnTile(nena, tiles.getTileLocation(6, 9))
 nena.ay = 350
 nena.setStayInScreen(true)
 scene.cameraFollowSprite(nena)
-//  Minas
+//  GENERADOR DE MINAS
 let lista_minas = tiles.getTilesByType(assets.tile`interrogacion`)
-for (let i = 0; i < lista_minas.length; i++) {
+let i = 0
+while (i < lista_minas.length) {
     lugar = lista_minas[i]
     nueva_minita = sprites.create(assets.image`minita3`, SpriteKind.Enemy)
     tiles.placeOnTile(nueva_minita, lugar)
-    tiles.setTileAt(lugar, assets.tile`transparency16`)
+    i += 1
 }
-// Salto Toldos
+//  GENERADOR DE TOLDOS
 let partes_toldo = [assets.tile`toldo01`, assets.tile`toldo02`, assets.tile`toldo03`, assets.tile`toldo04`]
-for (let t = 0; t < partes_toldo.length; t++) {
+let t = 0
+while (t < partes_toldo.length) {
     tipo_actual = partes_toldo[t]
     lista_lugares_toldo = tiles.getTilesByType(tipo_actual)
-    for (k = 0; k < lista_lugares_toldo.length; k++) {
+    k = 0
+    while (k < lista_lugares_toldo.length) {
         lugar_t = lista_lugares_toldo[k]
         nuevo_toldo = sprites.create(tipo_actual, SpriteKind.Trampolin)
         tiles.placeOnTile(nuevo_toldo, lugar_t)
         tiles.setTileAt(lugar_t, assets.tile`transparency16`)
+        k += 1
     }
+    t += 1
 }
 //  CUENTA REGRESIVA
-for (k = 0; k < 3; k++) {
+controller.moveSprite(nena, 0, 0)
+k = 0
+while (k < 3) {
     numero = 3 - k
     if (nena) {
-        nena.sayText("" + ("" + numero), 1000, true)
+        nena.sayText("" + numero, 1000, true)
     }
     
     pause(1000)
+    k += 1
 }
 if (nena) {
     nena.sayText("¡CORRE!", 500, true)
 }
 
+juego_empezado = true
 controller.moveSprite(nena, 100, 0)
+if (controller.right.isPressed()) {
+    animation.runImageAnimation(nena, assets.animation`maduro-right0`, 200, true)
+} else if (controller.left.isPressed()) {
+    animation.runImageAnimation(nena, assets.animation`maduro-left`, 200, true)
+}
+
+//  CONTROLES
 controller.right.onEvent(ControllerButtonEvent.Pressed, function on_right_pressed() {
     if (nena) {
         animation.runImageAnimation(nena, assets.animation`maduro-right0`, 200, true)
@@ -87,12 +102,54 @@ function on_a_pressed() {
 
 controller.A.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
 controller.up.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
+//  LÓGICA IA + SUELOS
+let tiles_petroleo = [assets.tile`petroleo0`, assets.tile`petroleo02`, assets.tile`petroleo1`]
+let tile_mina = assets.tile`interrogacion`
 game.onUpdate(function on_on_update() {
+    let loc_actual: tiles.Location;
+    let columna: number;
+    let fila_abajo: number;
+    let ubicacion_suelo: tiles.Location;
+    let imagen_suelo: Image;
+    let estoy_en_petroleo: boolean;
     
     if (!nena || !bot) {
         return
     }
     
+    if (juego_empezado) {
+        loc_actual = nena.tilemapLocation()
+        columna = loc_actual.column
+        fila_abajo = loc_actual.row + 1
+        ubicacion_suelo = tiles.getTileLocation(columna, fila_abajo)
+        imagen_suelo = tiles.tileImageAtLocation(ubicacion_suelo)
+        //  1. MINAS
+        if (imagen_suelo == tile_mina) {
+            game.over(false)
+            return
+        }
+        
+        //  2. PETRÓLEO
+        estoy_en_petroleo = false
+        for (let aceite of tiles_petroleo) {
+            if (imagen_suelo == aceite) {
+                estoy_en_petroleo = true
+                break
+            }
+            
+        }
+        if (nena.isHittingTile(CollisionDirection.Bottom)) {
+            if (estoy_en_petroleo) {
+                controller.moveSprite(nena, 20, 0)
+            } else {
+                controller.moveSprite(nena, 100, 0)
+            }
+            
+        }
+        
+    }
+    
+    //  IA DEL BOT
     distancia3 = Math.abs(nena.x - bot.x)
     if (distancia3 > 90) {
         velocidad3 = 320
@@ -126,7 +183,8 @@ game.onUpdate(function on_on_update() {
     }
     
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_enemy_overlap(sprite: Sprite, otherSprite: Sprite) {
+//  COLISIONES
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function on_enemy_overlap(sprite2: Sprite, otherSprite2: Sprite) {
     game.over(false)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Trampolin, function on_toldo_overlap(sprite: Sprite, otherSprite: Sprite) {
